60101207b67a49268dd7b2b5277e9cd026d20c10
Yili <Yili.code@gmail.com>
2025-07-30 11:24:36 +0800

    Update(medium/0047)

---
+++ medium/0047-Permutations-II/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+  vector<vector<int>> result;
+  vector<int> path;
+
+  void backtracing(vector<int> &nums, vector<bool> &used) {
+    // termination condition
+    if (path.size() == nums.size()) {
+      result.push_back(path);
+      return;
+    }
+
+    // recursion
+    for (int i = 0; i < nums.size(); ++i) {
+      if (used[i]) continue;
+
+      if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+        continue;
+
+      path.push_back(nums[i]);
+      used[i] = true;
+      backtracing(nums, used);
+      used[i] = false;
+      path.pop_back();
+    }
+  }
+
+  vector<vector<int>> permuteUnique(vector<int> &nums) {
+    result.clear();
+    path.clear();
+    sort(nums.begin(), nums.end());
+    vector<bool> used(nums.size(), false);
+    backtracing(nums, used);
+    return result;
+  }
+};
