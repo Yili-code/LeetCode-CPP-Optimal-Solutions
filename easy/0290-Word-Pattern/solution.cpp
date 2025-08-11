66b8cac0da31fe20045a3704e93d5e83d038c859
Yili-code <Yili.code@gmail.com>
2025-08-11 23:02:53 +0800

    Update solution.cpp

--- easy/0290-Word-Pattern/solution.cpp
+++ easy/0290-Word-Pattern/solution.cpp
@@ -4,6 +4,7 @@ public:
     unordered_map<char, string> um;         // pattern -> string
     unordered_map<string, char> reverse_um; // string -> pattern
 
+    // convert strging s into vector<string>
     vector<string> words;
     string temp;
     for (int i = 0; i <= s.size(); ++i) {
@@ -17,9 +18,11 @@ public:
       }
     }
 
+    // preCheck
     if (words.size() != pattern.size())
       return false;
 
+    // bidirection mapping
     for (int i = 0; i < pattern.size(); ++i) {
       char ch = pattern[i];
       string word = words[i];
